c7356af4c943d8335df41d13576e21b61172608c
Delieltom <delieltom@unipam.edu.br>
2025-05-03 17:28:18 -0300

    Frontend Especialistas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "@pages/Home/Home";
 import Pacientes from "@pages/Pacientes";
+import Especialistas from "@pages/Especialistas/Especialistas";
 import Login from "@pages/Login/Login";
 import '@fontsource/inter'; 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/pacientes" element={<Pacientes />}/>
+          <Route path="/especialistas" element={<Especialistas />}/>
         </Route>       
         
       </Routes>
