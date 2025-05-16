b0667cd0bed65d76aedf6d2c6a7df33c9e69c190
lauanegs <130411983+lauanegs@users.noreply.github.com>
2025-05-16 20:08:36 -0300

    fix: correções de merge

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ import Login from "@pages/Login/Login";
 import "@fontsource/inter";
 import Financas from "@pages/Financas/Financas";
 import Calendario from "@pages/Agendamentos/Agendamentos";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-
-import '@fontsource/inter';
-
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 
@@ -27,6 +22,7 @@ function App() {
             {/* Rotas dentro do layout */}
             <Route path="/" element={<Layout />}>
               <Route path="/home" element={<Home />} />
+              <Route path="/agendamentos" element={<Calendario />} />
               <Route path="/pacientes" element={<Pacientes />}/>
               <Route path="/especialistas" element={<Especialistas />}/>
               <Route path="/financas" element={<Financas />} />
