be41c6a22bda46e888f77c51850a810d1506d5a5
Gabriel <gabriel.silva@gruposetta.com>
2025-05-28 08:47:53 -0300

    Atualizado componente de novo especialista

--- src/components/cardAlerta/index.tsx
+++ src/components/cardAlerta/index.tsx
@@ -2,25 +2,25 @@ import React from "react";
 import "./cardAlerta.css";
 
 interface CardProps {
-    id: number;
-    titulo: string;
-    conteudo: string;
-    acao: () => void;
+	id: number;
+	titulo: string;
+	conteudo: string;
+	acao: () => void;
 }
 
 const CardAlerta: React.FC<CardProps> = ({ titulo, conteudo, acao }) => {
-    return (
-        <div className="card">
-            <div className="card-content">
-                <h5 className="card-title">{titulo}</h5>
-                <p className="card-description">{conteudo}</p>
-            </div>
+	return (
+		<div className="card">
+			<div className="card-content">
+				<h5 className="card-title">{titulo}</h5>
+				<p className="card-description">{conteudo}</p>
+			</div>
 
-            <button onClick={acao} className="card-button">
-                Fechar
-            </button>
-        </div>
-    );
+			<button onClick={acao} className="card-button">
+				Fechar
+			</button>
+		</div>
+	);
 };
 
 export default CardAlerta;
